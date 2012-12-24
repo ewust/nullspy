45c446ed2f698ea8e275039c4bbdf9c395283480
Eric Wustrow <ewust@umich.edu>
2012-12-23 23:22:53 -0800

    Make module act similar to current /dev/null
    Implemented writes; still have to do reads

--- nullspy.c
+++ nullspy.c
@@ -1,15 +1,46 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+
+
+static ssize_t
+nullspy_write(struct file *filp, const char __user *buf,
+              size_t len, loff_t *off)
+{
+    return len;
+}
+
+struct file_operations nullspy_fops = {
+    .read = NULL,
+    .write = nullspy_write,
+    .open = NULL,
+    .release = NULL
+};
+
+
+static int nullspy_major;
+#define NULLSPY_NAME "nullspy"
+
 
 static int __init nullspy_start(void)
 {
-    printk(KERN_INFO "Hello world\n");
+
+    nullspy_major = register_chrdev(0, NULLSPY_NAME, &nullspy_fops);
+    if (nullspy_major < 0) {
+        printk(KERN_ALERT "Registering char device failed with %d\n",
+               nullspy_major);
+        return nullspy_major;
+    }
+    printk(KERN_INFO "Registered nullspy major %d\n", nullspy_major);
+
+
     return 0;
 }
 
 static void __exit nullspy_end(void)
 {
+    unregister_chrdev(nullspy_major, NULLSPY_NAME);
     printk(KERN_INFO "Unloading\n");
 }
 
